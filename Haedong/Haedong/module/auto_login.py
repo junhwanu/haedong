b9a98695d77284e238f02f8e6be91e9647e42e13
yonggon_park <pyonggon11@naver.com>
2017-07-14 16:19:54 +0900

    mod auto login(2)

--- Haedong/Haedong/module/auto_login.py
+++ Haedong/Haedong/module/auto_login.py
@@ -122,7 +122,7 @@ class Login(threading.Thread):
         while True:
             print('트레이 아이콘을 찾는 중입니다.')
             #아이콘위치 = pyautogui.locateCenterOnScreen(아이콘, region=(화면x - 400, 화면y - 100, 400, 100), tolerance =50)
-            아이콘위치 = pyautogui.locateCenterOnScreen(아이콘, region=(화면x - 400, 화면y - 100, 400, 100), confidence=.5)
+            아이콘위치 = pyautogui.locateCenterOnScreen(아이콘, region=(화면x - 400, 화면y - 100, 400, 100), confidence=.7)
 
             if 아이콘위치:
                 print('트레이 아이콘을 찾았습니다.')
@@ -132,11 +132,11 @@ class Login(threading.Thread):
                 pyautogui.click(x, y, button='right')
                 break
 
-        time.sleep(2)
+        time.sleep(1)
         # 등록 버튼 누르기
         print('등록버튼 누르기')
-        pyautogui.moveRel(10, -35)
-        pyautogui.click()
+        # pyautogui.moveRel(10, -35)
+        pyautogui.click(x+10, y-35)
 
         time.sleep(2)
 
